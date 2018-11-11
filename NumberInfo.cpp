5a4aa47b91586412b2a96883b65e84f617547481
FatehinSiddique20 <fatehin20@gmail.com>
2018-11-11 22:25:07 +0600

    Setter getter for mobile number and getter for mobile operator is added

--- NumberInfo.cpp
+++ NumberInfo.cpp
@@ -18,3 +18,17 @@ void NumberInfo::set_number_operator(string numberOperator)
 {
 	this->numberOperator = numberOperator;
 }
+string NumberInfo::get_mobile_number()
+{
+	return mobileNumber;
+}
+
+string NumberInfo::get_number_operator()
+{
+	return numberOperator;
+}
+
+void NumberInfo::set_mobile_number(string mobileNumber)
+{
+	this->mobileNumber = mobileNumber;
+}
