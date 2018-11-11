4de9f4f0374889b08ec5def409491a6a8b6526af
FatehinSiddique20 <fatehin20@gmail.com>
2018-11-11 21:45:22 +0600

    Header file for NumberInfo is added.

---
+++ NumberInfo.h
@@ -0,0 +1,28 @@
+#ifndef NUMBERINFO_H_INCLUDED
+#define NUMBERINFO_H_INCLUDED
+
+
+#include<iostream>
+using namespace std;
+class NumberInfo
+{
+public:
+	NumberInfo();
+	NumberInfo(string,string);
+
+	~NumberInfo();
+	void set_mobile_number(string);
+	void set_number_operator(string);
+	bool is_equal(NumberInfo);
+
+	string get_mobile_number();
+	string get_number_operator();
+
+private:
+	string mobileNumber;
+	string numberOperator;
+};
+
+
+
+#endif // NUMBERINFO_H_INCLUDED
