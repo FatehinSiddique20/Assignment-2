5125d538d984280b778f6b6063ae61e78fe40fe6
FatehinSiddique20 <fatehin20@gmail.com>
2018-11-11 23:21:09 +0600

    Created a phone class for adding removing and searching phonenumber

---
+++ PhoneNumber.h
@@ -0,0 +1,28 @@
+#ifndef PHONENUMBER_H_INCLUDED
+#define PHONENUMBER_H_INCLUDED
+#include<string>
+#include<iostream>
+#include"UnsortedType.h"
+#include "NumberInfo.h"
+
+using namespace std;
+
+class PhoneNumber
+{
+public:
+	PhoneNumber();
+	PhoneNumber(string, string);
+	~PhoneNumber();
+	void add_phone_number(string, string);
+	void remove_phone_number(string, string);
+	bool search_phone_number(string, string);
+
+private:
+	UnsortedType<NumberInfo> numberList;
+
+
+};
+
+
+
+#endif // PHONENUMBER_H_INCLUDED
