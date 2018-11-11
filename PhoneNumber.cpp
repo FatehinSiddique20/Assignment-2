c8f955cfc8d3df45172d140a49e24d1a328ae639
FatehinSiddique20 <fatehin20@gmail.com>
2018-11-12 00:15:07 +0600

    Add phone number and Parmatrised constructor had the block code, hence have called the add_phone_number to the par constructor, used the object created for unsorted type linked list to check the length of the numbers inputed, in the remove phone number function, I have called the object for unsorted type class to see if the phone number I'm removing is the last number of the customer if yes, then I've called the make empty function to delete all the dat of the customer.

--- PhoneNumber.cpp
+++ PhoneNumber.cpp
@@ -7,15 +7,7 @@ PhoneNumber::PhoneNumber()
 
 PhoneNumber::PhoneNumber(string mobileNumber, string numberOperator)
 {
-	NumberInfo  number(mobileNumber,numberOperator);
-	if (numberList.LengthIs() == 15)
-	{
-		cout << "Already have 15 numbers!" << endl;
-	}
-	else
-	{
-		numberList.InsertItem(number);
-	}
+	add_phone_number(string mobileNumber, string numberOperator);
 	
 
 }
@@ -24,4 +16,34 @@ PhoneNumber::PhoneNumber(string mobileNumber, string numberOperator)
 PhoneNumber::~PhoneNumber()
 {
 
+}
+
+void PhoneNumber::add_phone_number(string mobileNumber, string numberOperator)
+{
+	NumberInfo  number(mobileNumber, numberOperator);
+	if (numberList.LengthIs() == 15)
+	{
+		cout << "Already have 15 numbers!" << endl;
+	}
+	else
+	{
+		numberList.InsertItem(number);
+	}
+
+
+
+}
+
+void PhoneNumber::remove_phone_number(string mobileNumber, string numberOperator)
+{
+
+	NumberInfo  number(mobileNumber, numberOperator);
+	if (numberList.LengthIs() == 1)
+	{
+		numberList.MakeEmpty();
+	}
+	else
+	{
+		numberList.DeleteItem(number);
+	}
 }
