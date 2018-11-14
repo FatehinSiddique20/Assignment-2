ce64d996140e8f14ad11d88f674f8ba8ae1e3aa7
FatehinSiddique20 <fatehin20@gmail.com>
2018-11-14 09:37:48 +0600

    Added constructor to person class

---
+++ Person.cpp
@@ -0,0 +1,21 @@
+#include "Person.h"
+#include<iostream>
+using namespace std;
+
+Person::Person()
+{
+	 name = "";
+	 fatherName = "";
+	 motherName = "";
+	 maritalStatus;
+	 nationalId = "";
+	 spouseName = "";
+	 presentAddress = "";
+	 permanentAddress = "";
+	 eyeColor = "";
+	 hairColor = "";
+         height = 0.0;
+	 weight = 0.0;
+	 
+
+}
