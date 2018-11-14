e4d4aafbf92111f309d2df9a960670e8ca838b89
FatehinSiddique20 <fatehin20@gmail.com>
2018-11-14 18:24:31 +0600

    Added parametrised constructor and destructor to person class

--- Person.cpp
+++ Person.cpp
@@ -14,8 +14,41 @@ Person::Person()
 	 permanentAddress = "";
 	 eyeColor = "";
 	 hairColor = "";
-         height = 0.0;
+         pheight = 0.0;
 	 weight = 0.0;
 	 
 
+}
+Person::~Person()
+{
+	 name = "";
+	 fatherName = "";
+	 motherName = "";
+	 maritalStatus;
+	 nationalId = "";
+	 spouseName = "";
+	 presentAddress = "";
+	 permanentAddress = "";
+	 eyeColor = "";
+	 hairColor = "";
+         pheight = 0.0;
+	 weight = 0.0;
+	 
+
+}
+Person::Person(string name, string fatherName, string motherName, string maritalStatus, string nationalId, string spouseName, string presentAddress, string permanentAddress, float height, float weight, string eyeColor, string hairColor)
+{
+		this->name = name;
+		this->fatherName = fatherName;
+		this->motherName = motherName;
+		this->maritalStatus = maritalStatus;
+		this->nationalId = nationalId;
+		this->spouseName = spouseName;
+		this->presentAddress = presentAddress;
+		this->permanentAddress = permanentAddress;
+		this->height = height;
+		this->weight = weight;
+		this->eyeColor = eyeColor;
+		this->hairColor = name;
+	
 }
